cfece394e9ec497950a7c9269bd33e869b3cff0e
uva <yuvaranin@techaffinity.com>
2022-05-26 22:43:13 +0530

    Auto-generated commit [ci skip]

---
+++ assets/mockData/clientList.ts
@@ -0,0 +1,58 @@
+export const ClientListData=
+  {
+    "Items":[
+       {
+          "Id":646,
+          "Name":"Allegion MRC",
+          "IsActive":true
+       },
+       {
+          "Id":619,
+          "Name":"Allied 2 (ACE) - BEACON EXCHANGE",
+          "IsActive":true
+       },
+       {
+          "Id":578,
+          "Name":"AlliedCMS (AIM) - Beacon MARKETING",
+          "IsActive":true
+       },
+       {
+          "Id":618,
+          "Name":"Andy's Restaurant",
+          "IsActive":true
+       },
+       {
+          "Id":593,
+          "Name":"Anytime Fitness",
+          "IsActive":true
+       },
+       {
+          "Id":651,
+          "Name":"Anytime Fitness Australia",
+          "IsActive":true
+       },
+       {
+          "Id":679,
+          "Name":"Butler Manufacturing",
+          "IsActive":true
+       },
+       {
+          "Id":623,
+          "Name":"City Wide - City Market",
+          "IsActive":true
+       },
+       {
+          "Id":674,
+          "Name":"Cornerstone Building Brands - Anniversary Store",
+          "IsActive":true
+       },
+       {
+          "Id":644,
+          "Name":"Cox Know Your Numbers",
+          "IsActive":true
+       }
+    ],
+    "NextPageLink":null,
+    "Count":62
+
+}
